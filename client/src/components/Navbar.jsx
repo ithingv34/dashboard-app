30312a58f94731734e36a368ee0ece42f6072c8d
ithingv34 <ithingv34@gmail.com>
2023-01-10 18:08:15 +0900

    add sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,11 @@ import { setMode } from 'state';
 import profileImage from "assets/profile.jpg";
 import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 
-const Navbar = () => {
+
+const Navbar = ({
+    isSidebarOpen,
+    setIsSidebarOpen
+}) => {
 
     const dispatch = useDispatch();
     const theme = useTheme();
@@ -30,7 +34,7 @@ const Navbar = () => {
             <Toolbar sx={{ justifyContent: "space-between"}}>
                 {/* left side */}
                 <FlexBetween>
-                    <IconButton onClick={() => console.log('open/close sidebar' )}>
+                    <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                         <MenuIcon/>
                     </IconButton>
                     <FlexBetween
