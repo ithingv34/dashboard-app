23512d4d7990a0a8ee52b3289998d854b31c0fe7
ithingv34 <ithingv34@gmail.com>
2023-01-12 15:12:52 +0900

    user information for sidebar, navbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -196,7 +196,7 @@ const Sidebar = ({
                 </List>
               </Box>
     
-              {/* <Box position="absolute" bottom="2rem">
+              <Box position="absolute" bottom="2rem">
                 <Divider />
                 <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
                   <Box
@@ -230,7 +230,7 @@ const Sidebar = ({
                     }}
                   />
                 </FlexBetween>
-              </Box> */}
+              </Box>
             </Drawer>
           )}
         </Box>
